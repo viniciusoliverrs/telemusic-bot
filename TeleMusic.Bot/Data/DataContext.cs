932b5f204332c2c84a7da1c29648cfbd1eb29295
Vinicius Oliveira <30914699+viniciusoliverrs@users.noreply.github.com>
2022-03-01 16:04:25 -0300

    feat: Removendo entidades apagadas

--- TeleMusic.Bot/Data/DataContext.cs
+++ TeleMusic.Bot/Data/DataContext.cs
@@ -6,9 +6,7 @@ namespace TeleMusic.Bot.Data
     {
         public DataContext() { }
         public DbSet<Video> Videos { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Chat> Chats { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-                                => options.UseSqlite("Data Source = Videos.db");
+                                => options.UseSqlite("Data Source = TeleMusic.db");
     }
 }
